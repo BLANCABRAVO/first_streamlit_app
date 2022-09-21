35f3c73271ced6a743cdbb96ae5e334391f04e86
Blanca Bravo <114015104+BLANCABRAVO@users.noreply.github.com>
2022-09-21 12:15:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
-
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boild Free_Range Eggs')
+
+streamlit.header('Breakfast Favourites')
+
+streamlit.text('🥣 Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boild Free_Range Eggs')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie🥝🍇')
